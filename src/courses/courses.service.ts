83c066d9c02c325b07eab9cd072a69a89958edc7
Mauricio Araujo Silva <mauricioas@weg.net>
2023-01-17 10:18:10 -0300

    Seção 1 - aula 11, Trabalhando com serviços

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: "Fundamentos do framework NestJS",
+            description: "Fundamentos do framework NestJS",
+            tags: ["node.js", "javascript", "nestjs"],
+        },
+    ];
+
+    findAll() {
+        return this.courses;
+    }
+
+    findOne(id: string) {
+        return this.courses.find((course) => course.id == Number(id));
+    }
+
+    create(createCourseDto: any) {
+        return this.courses.push(createCourseDto);
+    }
+
+    update(id: string, updateCourseDto: any) {
+        const indexCourse = this.courses.findIndex(
+            (course: Course) => course.id == Number(id),
+        );
+
+        this.courses[indexCourse] = updateCourseDto;
+    }
+
+    remove( id: string) {
+        const indexCourse = this.courses.findIndex(
+            (course: Course) => course.id == Number(id),
+        );
+
+        if ( indexCourse >= 0) {
+            this.courses.splice( indexCourse, 1);
+        }
+    }
+}

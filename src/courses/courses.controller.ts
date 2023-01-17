dd355e44401c37fc8ac9b1e21f168a443773cb13
Mauricio Araujo Silva <mauricioas@weg.net>
2023-01-17 09:24:28 -0300

    Seção 1 - aula 9, Customizando o status code das respostas

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('courses')
 export class CoursesController {
     @Get()
-    findAll() {
-        return 'Listagem de cursos';
+    findAll(@Res() response) {
+        return response.status(200).send('Listagem de cursos');
     }
 
     @Get(':id')
@@ -13,6 +14,7 @@ export class CoursesController {
     }
 
     @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
     create(@Body('price') body) {
         return body;
     }
